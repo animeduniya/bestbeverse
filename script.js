05869c8164e38a9d609bb17ff78881daffc54c04
animeduniya <duniyabarbaad24@gmail.com>
2025-02-28 11:17:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-// Show the popup only on the homepage when the site is first loaded or reloaded
-window.onload = function() {
-    if (window.location.pathname === '/index.html' || window.location.pathname === '/') {
-        document.getElementById("popup").style.display = 'block';
-
-        // Close the popup when the button is clicked
-        document.getElementById("close-popup").onclick = function() {
-            document.getElementById("popup").style.display = 'none';
-        };
-    }
-};
-
 // Debounce function to limit the rate at which search function is called
 function debounce(func, wait) {
     let timeout;
